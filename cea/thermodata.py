3e71e4bb4c7a6f2976eecfc3e5e63e18498916cd
Alex Corrie <ajccode@gmail.com>
2014-04-01 11:56:39 +0100

    Naive low-level implementation of dimensionless property functions

--- cea/thermodata.py
+++ cea/thermodata.py
@@ -4,6 +4,7 @@ This module is built on top of the `thermoinp` module (which serves to
 provide low-level access to the database).
 
 """
+from math import log
 import collections
 
 Interval = collections.namedtuple('Interval', 
@@ -16,3 +17,45 @@ Species = collections.namedtuple('Species',
 								  'molar_mass',
 								  'heat_formation', 
 								  'intervals'])
+
+
+def _dimless_heat_capacity(T, a):
+	# Returns the dimensionless heat capacity, Cp/R
+	# T : Temperature, K
+	# a : coefficients, len(a) == 7
+	return (  a[0] / T**2 
+	        + a[1] / T
+	        + a[2]
+	        + a[3] * T
+	        + a[4] * T**2
+	        + a[5] * T**3
+	        + a[6] * T**4
+	        ) 
+
+def _dimless_enthalpy(T, a, b):
+	# Returns the dimensionless enthalpy, H/RT
+	# T : Temperature, K
+	# a : coefficients, len(a) == 7
+	# b : integration constant
+	return (- a[0] / T**2
+	        + (a[1] * log(T) + b) / T
+	        + a[2]
+	        + a[3] * T / 2.0 
+	        + a[4] * T**2 / 3.0 
+	        + a[5] * T**3 / 4.0 
+	        + a[6] * T**4 / 5.0
+	        )
+
+def _dimless_entropy(T, a, b):
+	# Returns the dimensionless entropy, S/R.
+	# T : Temperature, K
+	# a : coefficients, len(a) == 7
+	# b : integration constant
+	return (- coeffs[0] / T**2 / 2.0 
+	        - coeffs[1] / T 
+	        + (coeffs[2] * log(T) + consts[1])
+	        + coeffs[3] * T
+	        + coeffs[4] * T**2 / 2.0 
+	        + coeffs[5] * T**3 / 3.0 
+	        + coeffs[6] * T**4 / 4.0
+	        )
